b8a1bde9b881d1a0cf6010af70844c49da14f828
Tom McEachan <38360055+TomMcEachan@users.noreply.github.com>
2023-05-09 10:38:17 +0100

    updated config

--- client/next.config.js
+++ client/next.config.js
@@ -4,9 +4,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 });
 
-const nextConfig = {
-    output: "standalone",
-};
+const nextConfig = {};
 
 const withPWAModule = require("next-pwa")({
     dest: "public",
