7965c54d4c110831439a8c0ad3c5b03aa3b6c69d
Tom McEachan <38360055+TomMcEachan@users.noreply.github.com>
2023-07-09 23:57:40 +0100

    Updated state machine to save theme mode option to local storage

--- apps/client/src/utils/state/ColorModeMachine.ts
+++ apps/client/src/utils/state/ColorModeMachine.ts
@@ -1,24 +1,59 @@
-import { createMachine } from 'xstate'
+import { createMachine } from "xstate";
 
 export const ColorModeMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QGMD2AbVAnAwgCwEMA7GAOgBECsBrAYgBUB5AcWYBkBRAbQAYBdRKAAOqWAEsALmNRFBIAB6IAjAHYAHKTUAWAMwqArABoQAT0RqlpfQF9rxtJlyESYUmzFQ8Ehi3bd+ciLiUjJyigiqGtp6RqbK+iq2diBEqBBwcg7Y+MQwgaKS0rJICogAtABsxmYIlbb2GNnOZO6eEvnBRWGIWgBM1eYqVmo6o2Pjo-UgWU65rpQ0HYWhJeG9AJxaAwjrPKQ8IxNHSdZAA */
-  createMachine({
-    predictableActionArguments: true,
-    initial: 'Light',
-    id: 'ChangeTheme',
-    schema: {
-      events: {} as { type: 'TOGGLE' }
-    },
-    states: {
-      Light: {
-        on: {
-          TOGGLE: 'Dark'
-        }
-      },
-      Dark: {
-        on: {
-          TOGGLE: 'Light'
-        }
-      }
-    }
-  })
+    /** @xstate-layout N4IgpgJg5mDOIC5QGEAWBDAdjAKqsAtmAHQAyAllKgC4DEOA8gOJOkCiA2gAwC6ioABwD2sctXJDM-EAA9EAZgBsATmIBWABzz5mgOyKALIY0GDAGhABPRAFoATAY3EuStXYCMy9+8Uf3AX38LNCxcfCJiABF0ACcAa3pmVk5eaWFRcUlpOQQDdzV1DU1lNTV5LXkudwtrBHddAvl3OzV8lo1dDWUuRUCgkEwhCDhpEOwwPEIwNJExCSkkWUR3eVVTXWV5XSr69w61GtttAta1RQ1FF1PfNUDgjHHJiIoqahmM+ezENWVVRR0mgZlHkDD07IcEDZQep3Fw8htynZ5A47HcQGMwlMorE4u85llFjk9rpiOtNttvLpiQcrLZoa04fVNhokSi+v4gA */
+    createMachine(
+        {
+            predictableActionArguments: true,
+            id: "ChangeTheme",
+            initial: "setLight",
+            schema: {
+                events: {} as { type: "TOGGLE" },
+            },
+            states: {
+                setLight: {
+                    on: {
+                        TOGGLE: {
+                            target: "Light",
+                            actions: ["setInitial"],
+                        },
+                    },
+                },
+                Light: {
+                    on: {
+                        TOGGLE: {
+                            target: "Dark",
+                            actions: ["setDark"],
+                            cond: () =>
+                                localStorage.getItem("theme") === "Light",
+                        },
+                    },
+                },
+                Dark: {
+                    on: {
+                        TOGGLE: {
+                            target: "Light",
+                            actions: ["setLight"],
+                            cond: () =>
+                                localStorage.getItem("theme") === "Dark",
+                        },
+                    },
+                },
+            },
+        },
+        {
+            actions: {
+                setDark: () => {
+                    localStorage.setItem("theme", "Dark");
+                },
+                setLight: () => {
+                    localStorage.setItem("theme", "Light");
+                },
+                setInitial: () => {
+                    if (!localStorage.getItem("theme")) {
+                        localStorage.setItem("theme", "Light");
+                    }
+                },
+            },
+        },
+    );
