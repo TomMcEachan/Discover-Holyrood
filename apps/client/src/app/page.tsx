ff3c482198f7ae59b44c8310c6f698fd945d675d
Tom McEachan <38360055+TomMcEachan@users.noreply.github.com>
2023-07-25 16:49:48 +0100

    fixed video responsiveness

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -4,6 +4,8 @@ import { PageTitle } from "@/components/ServerComponents/PageTitle/PageTitle";
 import { getAllData } from "@/utils/fetching/DataFetching";
 import { VideoPlayer } from "@/components/ClientComponents/VideoPlayer/VideoPlayer";
 import { GlobalSearchCard } from "@/components/ClientComponents/Cards/GlobalCard.tsx/GlobalSearchCard";
+import { VideoTextBlock } from "@/components/ClientComponents/VideTextBlock/VideoTextBlock";
+import { Suspense } from "react";
 
 export default async function Home() {
     const pageData = await getAllData();
@@ -17,13 +19,27 @@ export default async function Home() {
                 subtitle="Discover your Scottish Parliament"
             />
             <div className="divider" />
-            <div className="" id="video_section">
-                <div>
-                    <VideoPlayer
-                        url="https://res.cloudinary.com/tommceachan/video/upload/v1690210142/Welcome_9a60d6e642.mp4"
-                        imageUrl="https://res.cloudinary.com/tommceachan/image/upload/v1690218402/medium_Video_Thumbnail_d0edef0fe4.png"
-                    />
-                </div>
+            <div className="grid grid-cols-3 gap-4" id="video_section">
+                <VideoPlayer
+                    url="https://res.cloudinary.com/tommceachan/video/upload/v1690210142/Welcome_9a60d6e642.mp4"
+                    imageUrl="https://res.cloudinary.com/tommceachan/image/upload/v1690218402/medium_Video_Thumbnail_d0edef0fe4.png"
+                />
+
+                <VideoTextBlock title="Welcome to Discover Holyrood,">
+                    <div className="mx-4 my-4 sm:text-sm lg:text-lg xl:text-2xl 2xl:text-2xl">
+                        <p className="pb-4">
+                            Discover Holyrood is a new way to explore the
+                            Scottish Parliament.
+                        </p>
+                        <p>
+                            It's a digital experience that allows you to explore
+                            the Scottish Parliament from the comfort of your own
+                            home. You can explore the Parliament in 360°, watch
+                            videos, read articles and even learn about the
+                            Parliament in augmented reality.
+                        </p>
+                    </div>
+                </VideoTextBlock>
             </div>
             <div className="divider" />
             <div className="">
