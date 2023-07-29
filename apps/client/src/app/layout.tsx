65b73b0f0bc48f8cbcc3de97f43c3869093ddb51
Tom McEachan <38360055+TomMcEachan@users.noreply.github.com>
2023-07-29 22:18:09 +0100

    updated floating buttons.

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -3,10 +3,8 @@ import "./globals.css";
 import { ThemeProvider } from "@/utils/providers/Theme";
 
 // Import Components
-import { AppBar } from "@/components/AppBar/AppBar";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
 import { Navbar } from "@/components/NavBar/NavBar";
-import { GlobalSearchModal } from "@/components/Search/Global/GlobalSearchModal";
 import { FloatingButtons } from "@/components/Buttons/FloatingButtons/FloatingButtons";
 
 // Import Global State
@@ -60,15 +58,13 @@ export default function RootLayout({
                 <GlobalStateProvider>
                     <ThemeProvider>
                         <Navbar />
-                        <GlobalSearchModal />
+                        <FloatingButtons />
                         <div
                             id="content-section"
                             className="min-w-screen top-10 mx-4 mt-10 min-h-full pt-10"
                         >
                             {children}
                         </div>
-                        <FloatingButtons />
-                        {/* <AppBar />  */}
                         <Sidebar />
                     </ThemeProvider>
                 </GlobalStateProvider>
--- apps/client/src/components/Buttons/FloatingButtons/FloatingButtons.tsx
+++ apps/client/src/components/Buttons/FloatingButtons/FloatingButtons.tsx
@@ -24,7 +24,10 @@ export const FloatingButtons = (props: Props) => {
     };
 
     return (
-        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50">
+        <div
+            className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50"
+            tabIndex={1}
+        >
             <div className="relative">
                 {/* Small Fab Buttons */}
                 {showFAB ? (
